b539118864301648a81953c90f8df263f0c041e3
Jim Sverkmo <jim.sverkmo@m-c02cd3ngmd6p.lan>
2020-10-22 09:05:34 +0200

    chore(project): added my-app

---
+++ my-app/src/app/modules/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const materialModules = [
+  // Material modules will be added here as we need them
+];
+
+@NgModule({
+  imports: materialModules,
+  exports: materialModules
+})
+export class MaterialModule { }
